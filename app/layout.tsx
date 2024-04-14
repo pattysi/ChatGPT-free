d8647386e915578ea55d44596904d62d7199b787
pattysi <160124405+pattysi@users.noreply.github.com>
2024-04-14 14:59:38 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ import { GoogleTagManager } from "@next/third-parties/google";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "NextChat",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "ChatGPT公益网",
+  description: "免费试用ChatGPT，q群921551235",
   viewport: {
     width: "device-width",
     initialScale: 1,
@@ -22,7 +22,7 @@ export const metadata: Metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "NextChat",
+    title: "ChatGPT公益网",
     statusBarStyle: "default",
   },
 };
