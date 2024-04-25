acb966bbdf54d3be2d7408df9cf0599a090490e9
pattysi <160124405+pattysi@users.noreply.github.com>
2024-04-25 20:19:11 +0800

    Update exporter.tsx

--- app/components/exporter.tsx
+++ app/components/exporter.tsx
@@ -546,9 +546,9 @@ export function ImagePreviewer(props: {
           </div>
 
           <div>
-            <div className={styles["main-title"]}>NextChat</div>
+            <div className={styles["main-title"]}>ChatGPT公益网</div>
             <div className={styles["sub-title"]}>
-              github.com/Yidadaa/ChatGPT-Next-Web
+              github.com/pattysi/ChatGPT-free
             </div>
             <div className={styles["icons"]}>
               <ExportAvatar avatar={config.avatar} />
